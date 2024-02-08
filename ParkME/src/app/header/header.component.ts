742e4d293231e1cc99f1f5bdefa3052d24961128
cooladj <anthony.dewayne.jackson@gmail.com>
2024-02-08 01:59:22 -0500

    Created a Simply header and basic logic to tell if user is signed in or not.

---
+++ ParkME/src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  LoggedIn = false
+
+
+  signOut() {
+    // var auth2 = gapi.auth2.getAuthInstance();
+    // auth2.signOut().then(function () {
+    //   console.log('User signed out.');
+    // });
+  }
+}
