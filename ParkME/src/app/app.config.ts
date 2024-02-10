4ef70b358793c97bcc55d3656e5af10fda3b42bc
cooladj <anthony.dewayne.jackson@gmail.com>
2024-02-10 05:34:35 -0500

    added sign in fuction, token decode, and session storage

--- ParkME/src/app/app.config.ts
+++ ParkME/src/app/app.config.ts
@@ -1,9 +1,32 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+} from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
-};
+  providers: [provideRouter(routes), provideClientHydration(),
+    {
+
+
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '754588958744-8l1p80q3rr35vj0ccbk7vsvk6ep9iag9.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (error) => {
+          console.error(error);
+        }
+      } as SocialAuthServiceConfig
+    }
+    ]
+}
