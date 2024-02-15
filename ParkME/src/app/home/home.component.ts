aef517f73b8e4d3d7abe760b2d47a251f652f9d4
jovannypayan <152627126+jovannypayan@users.noreply.github.com>
2024-02-14 22:37:42 -0500

    debugging map thats not showing up

--- ParkME/src/app/home/home.component.ts
+++ ParkME/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -10,3 +9,27 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
 }
+
+async function initMap() {
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+  const map = new Map(document.getElementById('map') as HTMLElement, {
+    center: { lat: 37.42, lng: -122.1 },
+    zoom: 14,
+    mapId: '4504f8b37365c3d0',
+  });
+
+  const priceTag = document.createElement('div');
+  priceTag.className = 'price-tag';
+  priceTag.textContent = '$2.5M';
+
+  const marker = new AdvancedMarkerElement({
+    map,
+    position: { lat: 37.42, lng: -122.1 },
+    content: priceTag,
+  });
+}
+initMap();
+
