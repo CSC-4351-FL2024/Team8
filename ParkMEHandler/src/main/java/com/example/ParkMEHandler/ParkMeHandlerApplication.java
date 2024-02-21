e921506e9fb1e78162364bd87281348e8af61a73
cooladj <anthony.dewayne.jackson@gmail.com>
2024-02-21 00:57:27 -0500

    created backend and connected it to the postgres sever

---
+++ ParkMEHandler/src/main/java/com/example/ParkMEHandler/ParkMeHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParkMEHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkMeHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkMeHandlerApplication.class, args);
+	}
+
+}
---
+++ ParkMEHandler/src/test/java/com/example/ParkMEHandler/ParkMeHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParkMEHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkMeHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
