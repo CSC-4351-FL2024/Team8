79be377ea54f74b91992a2ccadeebea5b833eeb7
fayeyao <tyao2@student.gsu.edu>
2024-03-29 09:50:08 -0400

    added HeartbeatController

--- ParkMEHandler/src/main/java/com/example/ParkMEHandler/ParkMeHandlerApplication.java
+++ ParkMEHandler/src/main/java/com/example/ParkMEHandler/ParkMeHandlerApplication.java
@@ -3,11 +3,22 @@ package com.example.ParkMEHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 public class ParkMeHandlerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkMeHandlerApplication.class, args);
 	}
+}
 
+@RestController
+public class HeartbeatController {
+
+    @GetMapping("/heartbeat")
+    public String heartbeat() {
+        return "OK";
+    }
 }
